6f57481101170ec9815b1c458684c34f90c62ba5
Michael Sudirgo <michaelsudirgo2@gmail.com>
2022-10-18 08:38:01 +0700

    Adding table user data

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,14 +13,20 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) 
+        /*data di table ini disesuaikan dengan yang sudah dibuat di whimscal*/
+        {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password') -> nullable();/*di set nullable karena tidak butuh password*/
+            $table->string('avatar')->nullable();
+            $table->string('occupation')->nullable();/*di set null karena admin tidak ada occupation*/
+            $table->boolean('is_admin')->default(false);/*ini untuk user biasa jadi defaultnya false*/
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamps();/*timestamp akan terkonvert secara real time menjadi created_at, updated_at*/
+            $table->softDeletes();
         });
     }
 
