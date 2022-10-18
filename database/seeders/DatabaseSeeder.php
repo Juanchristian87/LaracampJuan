593d73983af511de48105dc37705dc5bc9cdff5e
Michael Sudirgo <michaelsudirgo2@gmail.com>
2022-10-18 11:48:44 +0700

    Make admin user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,8 @@ class DatabaseSeeder extends Seeder
         $this->call
         ([
             CampTableSeeder::class,/*ini untuk memanggil data di camp table seeder dan memasukkannya ke tabel*/
-            CampBenefitTableSeeder::class/*ini untuk memanggil data di camp benefit table seeder dan memasukkannya ke tabel*/
+            CampBenefitTableSeeder::class,/*ini untuk memanggil data di camp benefit table seeder dan memasukkannya ke tabel*/
+            AdminUserSeeder::class,/*ini untuk memanggil data di admin user seeder dan memasukkannya ke tabel*/
         ]);
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
