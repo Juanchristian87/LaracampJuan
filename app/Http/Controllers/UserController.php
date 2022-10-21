41be4bfc88cadb13628011789ef47e39056c348c
Michael Sudirgo <michaelsudirgo2@gmail.com>
2022-10-21 17:38:59 +0700

    Fixing integration socialite

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Auth;
 
 class UserController extends Controller
 {
@@ -19,6 +21,18 @@ class UserController extends Controller
 
     public function handleProviderCallback()
     {
-        return "provider callback";
+        $callback = Socialite::driver('google')->stateless()->user();/*ini untuk mengambil data dari email yang telah kita gunakan untuk login*/
+        $data = 
+        [
+            'name' => $callback ->getName(),
+            'email' => $callback ->getEmail(),
+            'avatar' => $callback ->getAvatar(),
+            'email_verified_At' =>date('Y-m-d H:i:s', time()),
+        ];
+        /*ini data yang diambil*/
+        $user = User::firstOrCreate(['email' => $data['email']], $data); /*ini untuk create email atau user baru yang terdaftar selain email default*/
+        Auth::login($user, true);
+        return redirect(route('welcome')); /*setelah login langsung balik ke welcome page*/
+        /*ini data yang diambil*/
     }
 }
