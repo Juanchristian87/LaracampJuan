309282af4ec7cf5e9b8a58ffb5c0ce3330f2b7e9
Michael Sudirgo <michaelsudirgo2@gmail.com>
2022-11-18 21:26:25 +0700

    Integration Landing Page

--- database/seeders/CampTableSeeder.php
+++ database/seeders/CampTableSeeder.php
@@ -21,7 +21,7 @@ class CampTableSeeder extends Seeder
         [
             [
                 'title' => 'Gila Belajar',
-                'slug' => 'gila-balajar',
+                'slug' => 'gila-belajar',
                 'price' => 280,
                 'created_at' => date('Y-m-d H:i:s', time()),
                 'updated_at' => date('Y-m-d H:i:s', time()),
