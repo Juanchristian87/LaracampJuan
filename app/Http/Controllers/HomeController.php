2d27fab37d6d5a2e287ce027356a1c162a9b456d
Michael Sudirgo <michaelsudirgo2@gmail.com>
2022-11-19 12:28:53 +0700

    Integration Dashboard Page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Checkout;
+use Auth;
 
 class HomeController extends Controller
 {
     public function dashboard()
     {
-        return view ('user.dashboard');
+        $checkouts = Checkout::with('Camp')->whereUserId((Auth::id()))->get();// ini untuk ambil nama user yang checkout dari id user yang login
+        return view ('user.dashboard', [
+            'checkouts' => $checkouts
+        ]);
     }
 }
--- app/Models/Checkout.php
+++ app/Models/Checkout.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 class Checkout extends Model
 {
@@ -13,5 +15,15 @@ class Checkout extends Model
     public function setExpiredattribute($value)
     {
         $this->attributes['expired'] = date('Y-m-t', strtotime($value));
+    }
+
+    /**
+     * Get the user that owns the Checkout
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function Camp(): BelongsTo
+    {
+        return $this->belongsTo(Camp::class);
     }
 }
